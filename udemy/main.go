2441a5761deeb74e19a8db5b0f317cbb9655bb36
Danny <talegas96@gmail.com>
2020-08-27 21:18:25 -0500

    nested loops

--- udemy/main.go
+++ udemy/main.go
@@ -7,8 +7,9 @@ import (
 func main() {
 	// for init; condition; post {}
 	for a := 0; a <= 10; a++ {
+		fmt.Printf("The outer loop: %d\n", a)
 		for j := 0; j < 3; j++ {
-			fmt.Printf("The outer loop: %d\t The inner loop: %d\n", a, j)
+			fmt.Printf("\t\t The inner loop: %d\n", j)
 		}
 	}
 }
