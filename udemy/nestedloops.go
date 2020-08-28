2ecd85de827353aabd2e5717df5a32a9b5aa9635
Danny <talegas96@gmail.com>
2020-08-27 21:46:50 -0500

    message through bash commit

--- udemy/nestedloops.go
+++ udemy/nestedloops.go
@@ -8,8 +8,8 @@ func main() {
 	// for init; condition; post {}
 	for a := 0; a <= 10; a++ {
 		fmt.Printf("The outer loop: %d\n", a)
-		for j := 0; j < 3; j++ {
-			fmt.Printf("\t\t The inner loop: %d\n", j)
+		for b := 0; b < 3; b++ {
+			fmt.Printf("\t\t The inner loop: %d\n", b)
 		}
 	}
 }
