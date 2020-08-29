ef167835cda0fd9ecbd040e6270658e64a7cc971
Danny <talegas96@gmail.com>
2020-08-28 20:13:18 -0500

    examples and different approaches

--- 02-for-singlecondition/main.go
+++ 02-for-singlecondition/main.go
@@ -5,12 +5,17 @@ import (
 )
 
 func main()  {
+
+	// First example
+
 	x:= 1
 	for x < 10 {
 		fmt.Println(x)
 		x++
 	}
 	fmt.Println("Done")
+
+	// Second example
 
 	y:=1
 	for {
@@ -21,4 +26,36 @@ func main()  {
 	y++
 	}
 	fmt.Println("Doner")	
+
+	// Third example
+	
+	z:=1
+	for {
+		z++	
+		if z > 100 {
+			break
+		}
+		if z%2 != 0 {
+			continue
+		}
+		fmt.Println(z)
+	}
+	fmt.Println("Donest")	
+
+
+	// Third example my take
+
+	d:=1
+	for {	
+	if d > 100 {
+		break
+	}
+	if d%2 != 0 {
+		d++
+		continue
+	}
+	fmt.Println(d)
+	d++
+	}
+	fmt.Println("Donest 2")	
 }
