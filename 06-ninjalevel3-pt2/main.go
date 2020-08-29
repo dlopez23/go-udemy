d28553d8da1a8a58d0b2c8e3a05d9aa51d5c421c
Danny <talegas96@gmail.com>
2020-08-29 12:12:56 -0500

    updated

--- 06-ninjalevel3-pt2/main.go
+++ 06-ninjalevel3-pt2/main.go
@@ -6,6 +6,9 @@ import (
 
 func main() {
 	for a := 65; a <= 90; a++ {
-		fmt.Printf("%#U\n%#U\n%#U\n\n", a, a, a)
+		fmt.Println(a)
+		for b := 0; b < 3; b++ {
+			fmt.Printf("\t%#U\n", a)
+		}
 	}
 }
